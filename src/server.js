48a7790e45184400f346f3085dcdcc7a7c8069b7
kjh4357 <kjh4357@gmail.com>
2022-01-25 03:19:46 +0900

    websocket, express server add and chat service

--- src/server.js
+++ src/server.js
@@ -1,1 +1,45 @@
-console.log('hello')
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({server});
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket)
+    socket["nickname"] = "Anon";
+    console.log("Connected to Server")
+    socket.on("close", () => {
+        console.log("DisConnected from the Browser")
+    });
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                console.log(message.payload)
+                break;
+        }
+    })
+    
+})
+
+
+server.listen(3000);
